d75c188b44578ea8ad94aea64088636ac42db90a
digitaldownloadden463-afk <digitaldownloadden463@gmail.com>
2026-07-06 21:32:40 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -15,7 +15,21 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384]
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.gamemonetize.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.gamepix.com'
+      },
+      {
+        protocol: 'https',
+        hostname: '**.gamepix.com'
+      }
+    ]
   },
   // Security headers
   async headers() {
